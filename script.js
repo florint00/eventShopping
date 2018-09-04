ddc361abd6f28530d5688d0bb3a2e7d4f37c71c2
florin_t <33556234+florint00@users.noreply.github.com>
2018-09-04 04:42:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+// taking our variable that we need to
+// perform functionality into our app
+
+
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
+// we also need a function to return our input value
+
 function inputLength(){
     return input.value.length;
 }
+
+// we also need a function to create that new element 
+// everytime we pass in something 
 
 function createListElement(){
     var li = document.createElement("li");
@@ -25,6 +34,14 @@ function addListAfterKeypress(event){
         }
 }
 
+// we have an event listener on button and input
+// so everytime when we click the button and respectively when we 
+// press enter(keyCode=13) some functionality start to happen
+// addListAfterClick for any click on the button and 
+// addListAfterKeypress for any input entered 
+
 button.addEventListener("click", addListAfterClick );
 
 input.addEventListener("keypress", addListAfterKeypress );
+
+// the functionality is the same in both cases
